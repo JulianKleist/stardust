4afb9536d0ac096438e091d9a55e7f721ac91fbb
JulianKleist <julianlopez.j.l.52@gmail.com>
2023-10-06 16:17:20 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import './App.css';
+import React, {useState, useEffect} from 'react';
+import logo from './assets/logo.png';
+import VideoPlayer from './components/VideoPlayer';
+import StdButton from './components/std-button';
+import Vl2 from './components/vl2';
+import ImageExpandable from './components/images';
+import Blog from './components/blog';
+import Empresas from './components/empresas';
+import Hl from './components/hl';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  const VideoTop = '570vh'
+  const VideoHeight = "315"
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+     <div className='main_container'>
+       <div className='navbar'>
+        <div className='logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+        <div className={`nav_items ${isOpen && "open"}`}>
+          <a href='#'>DGC</a>
+          <a href='#'>SERVICES</a>
+          <a href='#'>ECOMMERCE</a>
+          <a href='#'>CUSTOMERS</a>
+          <a href='#'>BLOG</a>
+          <a href='#'>CONTACT</a>
+        </div>
+        <div className={`nav_toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className='vl'></div>
+      <Vl2 VlTop={"58rem"} />
+      <div className={`hl ${isOpen && "open"}`} data-aos="fade-down" data-aos-duration="1500"></div>
+      <div className={`title ${isOpen && "open"}`}>
+        <h1 data-aos="fade-down" data-aos-duration="1500">Performance Digital</h1>
+        <p data-aos="fade-down" data-aos-duration="2000">completamente a Resultado.</p>
+      </div>
+      <div className={`company ${isOpen && "open"}`}>
+        <img src="http://placehold.it/100x100" data-aos="zoom-in" data-aos-duration="2000"></img>
+        <img src="http://placehold.it/100x100" data-aos="zoom-in" data-aos-duration="2000"></img>
+        <img src="http://placehold.it/100x100" data-aos="zoom-in" data-aos-duration="2000"></img>
+      </div>
+      <Empresas />
+      <div className='second_title'>
+        <h2 data-aos="fade-down" data-aos-duration="1500">Somos un verdadero</h2>
+        <p data-aos="fade-down" data-aos-duration="2000">socio para crecer.</p>
+      </div>
+      <Hl StdTop={"165vh"}/>
+      <div className='second_text'>
+        <p data-aos="fade-down" data-aos-duration="1500">Somos responsables del éxito de decenas de compañías que escalaron sus ventas en los últimos 3 años de forma agresiva y sin descanso. Hoy nuestra agenda de trabajo está completa con clientes de todo LATAM y USA que buscan esos mismos resultados. ¿Por qué? Simplemente porque nacimos para generar resultado.</p>
+      </div>
+      <VideoPlayer VideoWidth={"560"} VideoHeight={VideoHeight}/>
+      <p className='video-description' data-aos="fade-in" data-aos-duration="2000">Lorem ipsum dolor sit amet</p>
+      <StdButton stdText={'SOLICITAR PRESUPUESTO'} StdTop={"280vh"}/>
+      <Vl2 VlTop={"410vh"} />
+      <div className='third_title'>
+        <h2 data-aos="fade-down" data-aos-duration="1500">Nuestra oferta</h2>
+        <p data-aos="fade-down" data-aos-duration="2000">es simple y sencilla.</p>
+      </div>
+      <Hl StdTop={"386vh"}/>
+      <div className='third_text'>
+        <p data-aos="fade-down" data-aos-duration="2000">Conectamos a tu empresa un equipo AAA compuesto por especialistas en Paid Ads, Data Analytics, Publicidad, Diseño y Copywriting. Nuestro equipo pondrá en marcha nuestro framework de trabajo, el mismo que venimos corriendo para todos nuestros casos de éxito. En menos de doce semanas vas a experimentar un crecimiento significativo en tu facturación junto con un aumento en la notoriedad. ¿Qué garantía tenes? Más allá de nuestra extensa cartera de casos de éxito en comercio electrónico proponemos una modalidad única en donde si no vendés no pagás, vamos variabilizados completamente.</p>
+      </div>
+      <VideoPlayer VideoTop={VideoTop} VideoWidth={360} VideoHeight={"200"} VideoLeft={"12vw"}/>
+      <VideoPlayer VideoTop={VideoTop} VideoWidth={360} VideoHeight={"200"}/>
+      <VideoPlayer VideoTop={VideoTop} VideoWidth={360} VideoHeight={"200"}VideoLeft={"64vw"}/>
+      <Vl2 VlTop={"620vh"} />
+      <div className='casos'>
+        <h2 data-aos="fade-down" data-aos-duration="2000">Casos de éxito reales</h2>
+        <p data-aos="fade-down" data-aos-duration="2000">Todos nuestros clientes en las primeras 12 semanas experimentan un crecimiento enorme en sus ventas. Te presentamos alguno de nuestros casos de éxito, con capturas de 
+          pantalla de sus cuentas publicitarias.
+100% Resultados reales que podemos mostrarte en una llamada.</p>
+      </div>
+      <Hl StdTop={"515vh"}/>
+      <div className='images'>
+        <ImageExpandable src="http://placehold.it/600x300" width="1000px" height="500px" />
+      </div>
+      <Vl2 VlTop={"930vh"} />
+      <div className='consultoria'>
+        <h2 data-aos="fade-down" data-aos-duration="2000">Consultoría gratuita</h2>
+        <p data-aos="fade-down" data-aos-duration="2000">Agenda una videollamada de diagnóstico y consultoría totalmente gratuita. Nuestros especialistas realizarán un diagnóstico y te brindará una evaluación de posibles 
+          alterativas para tu plan de crecimiento!</p>
+      </div>
+      <Hl StdTop={"608vh"}/>
+      <div className='calendly'>
+        <div class="calendly-inline-widget" data-url="https://calendly.com/ejirouweghrepha"></div>
+        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+      </div>
+      <di className="blog">
+        <h2 data-aos="fade-down" data-aos-duration="3000">Nuestro Blog</h2>
+      </di>
+      <Blog />
+      <Hl StdTop={"600vh"}/>
+      <StdButton stdText={"VISITAR BLOG"} StdTop={"705vh"}/>
+     </div>
+    );
+}
+
+export default App;
